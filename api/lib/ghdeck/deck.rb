691ae3af5d75bc93ac227e2067a5eb4de83e819b
iagox86 <ron-git@skullsecurity.org>
2018-05-08 15:40:12 -0700

    When a new card is added, the remainder of the deck is now shuffled, rather than the entire deck. Required some structural changes, so tests had to change quite a bit.

--- api/lib/ghdeck/deck.rb
+++ api/lib/ghdeck/deck.rb
@@ -5,29 +5,35 @@ module GHDeck
 		class Exception < ::GHDeck::Exception
 		end
 
-		attr_accessor :shuffled
+		# These are available for testing
+		attr_accessor :shuffled, :reset
 
-		def initialize(actually_shuffle:true)
-			# This is purely for debugging
-			@actually_shuffle = actually_shuffle
-			@shuffled = false
-
-			@shuffle_at_end_of_round = false
-		end
-
-		# Shuffle the deck in-place and return to index 0
+		# Shuffle the remaining cards in the deck
 		def _shuffle!()
 			if(@actually_shuffle)
 				@deck.shuffle!()
 			end
 
-			@index = 0
-
 			# Testing
 			@shuffled = true
 		end
 
-		# Add a new card to the deck, which also triggers a shuffle
+		def _reset!()
+			# The order these are combined matters because of tests - they should
+			# preserve the original order
+			@deck = @discard + @deck
+
+			# Be sure to empty the discard pile when we do this
+			@discard = []
+			_shuffle!()
+
+			# Testing
+			@reset = true
+		end
+
+		# Add a new card to the available deck. This also triggers a shuffle of the
+		# available cards (but not a full reset of the deck, the discard pile stays
+		# where it is)
 		def add_card(name:, display:, rolling:, reshuffle:, remove:, **args)
 			@deck << {
 				name: name,
@@ -41,8 +47,12 @@ module GHDeck
 			_shuffle!()
 		end
 
-		def set_deck(deck)
+		def initialize(deck:, actually_shuffle:true)
+			# This is purely for debugging
+			@actually_shuffle = actually_shuffle
+
 			@deck = []
+			@discard = []
 			deck.each do |c|
 				begin
 					add_card(**c)
@@ -51,50 +61,62 @@ module GHDeck
 				end
 			end
 			_shuffle!()
+
+			# This will flip to `true` when a shuffle card is drawn (crit/miss)
+			@reset_at_end_of_round = false
+
+			# These are simply tracked for testing
+			@shuffled = false
+			@reset = false
 		end
 
 		# Draw a card
 		def roll()
+			# If we're out of cards, reset the deck
+			if(@deck.length == 0)
+				_reset!()
+			end
+
+			# If we're still out of cards, the deck was empty (oops!)
 			if(@deck.length == 0)
 				raise(Exception, "No cards in the deck!")
 			end
 
-			if(@index >= @deck.length)
-				_shuffle!()
+			# Remove the first card from the deck...
+			card = @deck.shift
+
+			# ...and add it to the discard deck if it's not a discard card
+			if(!card[:remove])
+				@discard << card
 			end
 
-			# TODO: Handle when the deck runs out of cards
-			card = @deck[@index]
-
-			# If the card is removed from the deck, do that; otherwise, go to the next
-			# card
-			if(card[:remove])
-				@deck.delete_at(@index)
-			else
-				@index += 1
-			end
-
+			# If the card triggers a reshuffle, queue that up
 			if(card[:reshuffle])
-				@shuffle_at_end_of_round = true
+				@reset_at_end_of_round = true
 			end
 
+			# Finally, return the card
 			return card
 		end
 
 		# Reset the deck (eg, between dungeons)
-		def reset_deck()
+		def reset_deck_to_clean()
+			# Reset (puts everything back into the @deck, empties out the @discard
+			# stack)
+			_reset!()
+
+			# Remove all the 'remove' cards from the deck
 			@deck.select!() { |c| !c[:remove] }
-			@index = 0
 		end
 
 		def end_round()
-			if(@shuffle_at_end_of_round)
-				_shuffle!()
+			if(@reset_at_end_of_round)
+				_reset!()
 			end
 		end
 
 		def get()
-			return @index, @deck.clone()
+			return @deck.clone(), @discard.clone()
 		end
 	end
 end
--- api/test/deck_test.rb
+++ api/test/deck_test.rb
@@ -4,72 +4,73 @@ require 'ghdeck/deck'
 
 module GHDeck
 	class DeckTest < ::Test::Unit::TestCase
-		def setup()
-			@test_cards = {
-				normal1:    { name: "normal1",    display: "n1",  rolling: false, reshuffle: false, remove: false },
-				normal2:    { name: "normal2",    display: "n2",  rolling: false, reshuffle: false, remove: false },
-				normal3:    { name: "normal3",    display: "n3",  rolling: false, reshuffle: false, remove: false },
+		TEST_CARDS = {
+			normal1:    { name: "normal1",    display: "n1",  rolling: false, reshuffle: false, remove: false },
+			normal2:    { name: "normal2",    display: "n2",  rolling: false, reshuffle: false, remove: false },
+			normal3:    { name: "normal3",    display: "n3",  rolling: false, reshuffle: false, remove: false },
 
-				rolling1:   { name: "rolling1",   display: "r1",  rolling: true,  reshuffle: false, remove: false },
-				rolling2:   { name: "rolling2",   display: "r2",  rolling: true,  reshuffle: false, remove: false },
-				rolling3:   { name: "rolling3",   display: "r3",  rolling: true,  reshuffle: false, remove: false },
+			rolling1:   { name: "rolling1",   display: "r1",  rolling: true,  reshuffle: false, remove: false },
+			rolling2:   { name: "rolling2",   display: "r2",  rolling: true,  reshuffle: false, remove: false },
+			rolling3:   { name: "rolling3",   display: "r3",  rolling: true,  reshuffle: false, remove: false },
 
-				reshuffle1: { name: "reshuffle1", display: "re1", rolling: false, reshuffle: true,  remove: false },
-				reshuffle2: { name: "reshuffle2", display: "re2", rolling: false, reshuffle: true,  remove: false },
-				reshuffle3: { name: "reshuffle3", display: "re3", rolling: false, reshuffle: true,  remove: false },
+			reshuffle1: { name: "reshuffle1", display: "re1", rolling: false, reshuffle: true,  remove: false },
+			reshuffle2: { name: "reshuffle2", display: "re2", rolling: false, reshuffle: true,  remove: false },
+			reshuffle3: { name: "reshuffle3", display: "re3", rolling: false, reshuffle: true,  remove: false },
 
-				remove1:    { name: "remove1",    display: "rm1", rolling: false, reshuffle: false, remove: true },
-				remove2:    { name: "remove2",    display: "rm2", rolling: false, reshuffle: false, remove: true },
-				remove3:    { name: "remove3",    display: "rm3", rolling: false, reshuffle: false, remove: true },
-			}
-
-			@deck = Deck.new(
-				# Turn shuffle off so we can make sure things at working
-				actually_shuffle: false,
-			)
-		end
+			remove1:    { name: "remove1",    display: "rm1", rolling: false, reshuffle: false, remove: true },
+			remove2:    { name: "remove2",    display: "rm2", rolling: false, reshuffle: false, remove: true },
+			remove3:    { name: "remove3",    display: "rm3", rolling: false, reshuffle: false, remove: true },
+		}
 
 		def test_draw_cards()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:normal2],
-				@test_cards[:normal3],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:normal2],
+				TEST_CARDS[:normal3],
 			])
 
 			# So we can make sure it's not shuffled
-			@deck.shuffled = false
+			deck.shuffled = false
+			deck.reset = false
 
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_false(@deck.shuffled)
-			assert_equal(@test_cards[:normal2], @deck.roll())
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 		end
 
 		def test_draw_past_end()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:normal2],
-				@test_cards[:normal3],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:normal2],
+				TEST_CARDS[:normal3],
 			])
 
 			# So we can make sure it's not shuffled
-			@deck.shuffled = false
+			deck.shuffled = false
+			deck.reset = false
 
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_false(@deck.shuffled)
-			assert_equal(@test_cards[:normal2], @deck.roll())
-			assert_false(@deck.shuffled)
-			assert_equal(@test_cards[:normal3], @deck.roll())
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
+			assert_equal(TEST_CARDS[:normal3], deck.roll())
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 
 			# This should trigger a shuffle
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_true(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_true(deck.shuffled)
+			assert_true(deck.reset)
 		end
 
 		def test_set_bad_cards()
 			e = assert_raises(::GHDeck::Deck::Exception) do
-				@deck.set_deck([
+				Deck.new(actually_shuffle: false, deck: [
 					{:a => 1}
 				])
 			end
@@ -78,146 +79,155 @@ module GHDeck
 		end
 
 		def test_add_card()
-			@deck.set_deck([
-				@test_cards[:normal1],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
 			])
 
 			# So we can make sure it's not shuffled
-			@deck.shuffled = false
+			deck.shuffled = false
+			deck.reset = false
 
-			@deck.add_card(
+			deck.add_card(
 				{ name: "test", display: "test", rolling: false, reshuffle: false, remove: false },
 			)
 
 			# Make sure it's shuffled
-			assert_true(@deck.shuffled)
+			assert_true(deck.shuffled)
+			assert_false(deck.reset)
 
 			# Burn the other card
-			@deck.roll()
+			deck.roll()
 
 			# Check if the card is inserted
-			assert_equal('test', @deck.roll()[:name])
+			assert_equal('test', deck.roll()[:name])
 		end
 
 		def test_end_round_normal()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:normal2],
-				@test_cards[:normal3],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:normal2],
+				TEST_CARDS[:normal3],
 			])
 
-			@deck.shuffled = false
+			deck.shuffled = false
+			deck.reset = false
 
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			@deck.end_round()
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			deck.end_round()
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 
-			assert_equal(@test_cards[:normal2], @deck.roll())
-			@deck.end_round()
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
+			deck.end_round()
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 
-			assert_equal(@test_cards[:normal3], @deck.roll())
-			@deck.end_round()
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal3], deck.roll())
+			deck.end_round()
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 		end
 
 		def test_remove()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:remove1],
-				@test_cards[:normal2],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:remove1],
+				TEST_CARDS[:normal2],
 			])
 
 			# Draw the three cards
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_equal(@test_cards[:remove1], @deck.roll())
-			assert_equal(@test_cards[:normal2], @deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_equal(TEST_CARDS[:remove1], deck.roll())
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
 
 			# After a shuffle happens, there should only be two cards
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_equal(@test_cards[:normal2], @deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
 		end
 
 		def test_end_round_shuffle()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:reshuffle1],
-				@test_cards[:normal2],
-				@test_cards[:reshuffle2],
+			deck = Deck.new(actually_shuffle: false, deck: [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:reshuffle1],
+				TEST_CARDS[:normal2],
+				TEST_CARDS[:reshuffle2],
 			])
 
-			@deck.shuffled = false
+			deck.shuffled = false
+			deck.reset = false
 
 			# End round after 1 normal card
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			@deck.end_round()
-			assert_false(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			deck.end_round()
+			assert_false(deck.shuffled)
+			assert_false(deck.reset)
 
 			# End second round after 1 reshuffle card
-			assert_equal(@test_cards[:reshuffle1], @deck.roll())
-			@deck.end_round()
-			assert_true(@deck.shuffled)
+			assert_equal(TEST_CARDS[:reshuffle1], deck.roll())
+			deck.end_round()
+			assert_true(deck.shuffled)
+			assert_true(deck.reset)
 
 			# Do three turns then end round
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_equal(@test_cards[:reshuffle1], @deck.roll())
-			assert_equal(@test_cards[:normal2], @deck.roll())
-			@deck.end_round()
-			assert_true(@deck.shuffled)
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_equal(TEST_CARDS[:reshuffle1], deck.roll())
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
+			deck.end_round()
+			assert_true(deck.shuffled)
+			assert_true(deck.reset)
 
 			# Make sure we're back at the start
-			assert_equal(@test_cards[:normal1], @deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
 		end
 
 		def test_reset_deck()
-			@deck.set_deck([
-				@test_cards[:normal1],
-				@test_cards[:remove1],
-				@test_cards[:normal2],
-				@test_cards[:remove2],
+			deck = Deck.new(actually_shuffle: false, deck:[
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:remove1],
+				TEST_CARDS[:normal2],
+				TEST_CARDS[:remove2],
 			])
 
 			# Draw a card to make sure it resets the index
-			assert_equal(@test_cards[:normal1], @deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
 
-			@deck.reset_deck()
+			deck.shuffled = false
+			deck.reset = false
+			deck.reset_deck_to_clean()
+			assert_true(deck.shuffled)
+			assert_true(deck.reset)
 
 			# Make sure it's just the two normal cards
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_equal(@test_cards[:normal2], @deck.roll())
-			assert_equal(@test_cards[:normal1], @deck.roll())
-			assert_equal(@test_cards[:normal2], @deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
+			assert_equal(TEST_CARDS[:normal1], deck.roll())
+			assert_equal(TEST_CARDS[:normal2], deck.roll())
 		end
 
 		def test_get()
-			deck = [
-				@test_cards[:normal1],
-				@test_cards[:normal2],
+			test_deck = [
+				TEST_CARDS[:normal1],
+				TEST_CARDS[:normal2],
 			]
 
-			@deck.set_deck(deck)
+			deck = Deck.new(actually_shuffle: false, deck: test_deck)
 
-			index, new_deck = @deck.get()
-			assert_equal(0, index)
-			assert_equal(deck, new_deck)
+			the_deck, the_discard = deck.get()
+			assert_equal(test_deck, the_deck)
+			assert_equal([], the_discard)
 
-			@deck.roll()
+			deck.roll()
 
-			index, new_deck = @deck.get()
-			assert_equal(1, index)
-			assert_equal(deck, new_deck)
+			the_deck, the_discard = deck.get()
+			assert_equal([TEST_CARDS[:normal2]], the_deck)
+			assert_equal([TEST_CARDS[:normal1]], the_discard)
 		end
 
 		def test_extra_card_args()
 			entry = { name: "normal1", display: "n1",  rolling: false, reshuffle: false, remove: false, test_field: 'abc' }
-			@deck.set_deck([entry])
+			deck = Deck.new(actually_shuffle: false, deck: [entry])
 
-			assert_equal(entry, @deck.roll())
-		end
-
-		def test_shuffle_remainder_of_deck_when_adding()
-			# TODO
-			assert_true(false)
+			assert_equal(entry, deck.roll())
 		end
 	end
 end
